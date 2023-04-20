f568b933fca4be3e3091f04d1dd2f693003e4446
Evgeniy <evgeniyklimow@gmail.com>
2023-04-20 10:08:09 +0300

    add new dz

--- Task_2_DZ.py
+++ Task_2_DZ.py
@@ -6,10 +6,17 @@
 123 -> 6 (1 + 2 + 3)
 100 -> 1 (1 + 0 + 0) |
 """""
-
+# 1 способ
 num = int(input("Введите трехзначное число: "))
 digit1 = num // 100
 digit2 = (num // 10) % 10
 digit3 = num % 10
 sum_of_digits = digit1 + digit2 + digit3
 print("Сумма цифр трехзначного числа", num, "равна", sum_of_digits)
+
+# 2 способ
+num = input('Введите число: ')
+num_sum = 0
+for i in num:
+    num_sum += int(i)
+print(num_sum)
